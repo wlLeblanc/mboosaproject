e944026d708309072ef03c81e71f0df6b35aba88
yangyuchao <1228004606@qq.com>
2016-09-21 22:05:04 +0800

    购物车基本完善

---
+++ src/js/cart.js
@@ -0,0 +1,64 @@
+var str = '[{product:"\u516C\u4E3B\u9774\u5B50", price:"RMB138",num:"1",img:"../images/xq_xiao_02.jpg"}, {product:"\u8776\u5986\u6D77\u84DD\u6E05\u6EE2\u7C89\u5E95\u6DB210", price:"RMB85",num:"1",img:"../images/xq_xiao_02.jpg"}]';
+
+// 将字符串重新执行，那么可以得到原来的数组
+//   因为可能存在安全问题
+//var arr = eval(str);
+
+// alert(arr.length)
+
+onload = function() {
+	var oUl = document.getElementById("plist");
+
+	var str = getCookie("arr");
+
+	// var a = "[{}]"
+	// string
+	//alert(typeof str);
+
+	var arr = eval(str);
+
+	for (var i = 0; i < arr.length; i++)
+	{
+		// 创建一个商品
+		var oLi = document.createElement("li");
+		//详细信息
+		var oSpan=document.createElement('span');
+		//合计金额
+		var oHj=document.createElement('em');
+		//数量
+		var oi=document.createElement('i');
+		//创建移除按钮
+		var oA=document.createElement('a');
+		//图片
+		var oImg=document.createElement('img');
+		var oDiv=document.createElement('h6');
+
+		// 取出数组中元素，arr[i] 是一个对象,
+		//    再取出这个对象中的产品名 和 价格
+		oA.innerHTML="移除"
+		oImg.src=arr[i].img
+		oi.innerHTML=arr[i].num;
+		oSpan.innerHTML=arr[i].price;
+		oDiv.innerHTML = arr[i].product;
+		oHj.innerHTML="￥"+(arr[i].num)*(arr[i].price)
+		//oLi.className("hov");
+		oLi.appendChild(oImg);
+		oLi.appendChild(oDiv);
+		oLi.appendChild(oSpan);
+		oLi.appendChild(oi);
+		oLi.appendChild(oHj);
+		oLi.appendChild(oA);
+		
+		oUl.appendChild(oLi);
+		
+	}
+	
+	$('a').on('click',function(){
+		$(this).parent().remove();
+	})
+	$('li').each(function(idx ,item){
+		var $i=$(this).$('i').html()
+		$('.pics').html("$i")
+		
+	})
+}
