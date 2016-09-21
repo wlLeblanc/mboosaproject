95f1b07158e1505205db3fffcd44c9bf861f8ae3
yangyuchao <1228004606@qq.com>
2016-09-21 16:42:27 +0800

    购物车cookie初步实现

---
+++ src/js/gouwu.js
@@ -0,0 +1,65 @@
+onload = function() {
+	var str = getCookie("arr");
+
+	// 用于存储所有的商品
+	var arr = [];
+	if (str != "")
+	{
+		// 说明之前 cookie 中有商品的内容
+		// 取出来转换成数组
+		arr = eval(str);
+	}
+	//var arr = [];
+	// 商品由 名称 和 价格组成
+	// 例如：
+	// {product: XXX, price: 180}
+	var allA = document.getElementById("payment_car");
+
+		allA.onclick = function() {
+			// 这个是价格
+			var oPrice = document.getElementById("price")
+
+			// 这个商品的信息
+			var oProduct =document.getElementById("product")
+			
+			
+			// 取出商品名字的标签对象
+			var oSpan = oProduct.getElementsByTagName("span")[0];
+		
+			// 取出标签对象中的名字，也就是商品的名称
+
+			// 函数库中自定义的函数
+			// 添加了一个 cookie,名字是 product, 内容是 商品的信息, 过期时间是 7天后
+			//alert(oSpan.innerHTML);
+			
+			// 转换成一个
+			// var str = encodeURI(oSpan.innerHTML);
+
+			var obj = {};
+
+			obj.product = oProduct.innerHTML;
+			obj.price = oPrice.innerHTML;
+
+			// 将创建好的商品添加到数组中
+			arr.push(obj);
+
+			// 将数组的内容设置到 cookie 中呢？
+			// cookie 的名字是 arr, 内容是数组中的商品，过期时间是7天以后
+			addCookie("arr", arr.toSource(), 7);
+
+
+
+			/*
+			addCookie("product", oSpan.innerHTML, 7);
+			
+			// 函数库中自定义的函数
+			// 添加了一个 cookie,名字是 price, 内容是 商品的价格, 过期时间是 7天后 
+			addCookie("price", oPrice.innerHTML, 7);
+			//*/
+			
+
+			alert("添加成功");
+		
+		}
+
+}
